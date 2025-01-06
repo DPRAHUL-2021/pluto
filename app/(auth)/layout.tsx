8a54384f5554c3bf5d3b3175b64a0e59adc72d2b
DPRAHUL <dpranavr2021@gmail.com>
2025-01-07 01:01:21 +0530

    Layout for Auth pages done

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,18 +4,32 @@ import Image from 'next/image';
 const layout = ({ children }: { children: React.ReactNode }) => {
     return (
         <div className='flex min-h-screen'>
-            <section className='bg-brand p-10'>
-                <div>
-                    <Image src="/favicon.ico" alt="Pluto" width={16} height={16} className="h-auto"/>
-                    
-                    <div className='space-y-5'>
+            <section className='hidden w-1/2 items-center justify-center bg-brand p-10 lg:flex xl:w-2/5'>
+                <div className='flex max-h-[800px] max-w-[430px] flex-col justify-center space-y-12'>
+                    <Image src="/assets/icons/logo-full.svg" alt="Pluto" width={224} height={82} className="h-auto" />
+
+                    <div className='space-y-5 text-white'>
                         <h1 className='h1'>Efficiently organize and access your files</h1>
                         <p className='body-1'>This is a space to safely keep all your documents.</p>
 
                     </div>
+
+                    <Image src="/assets/images/new_files.svg" alt="Pluto" width={342} height={342} className="transition-all hover:rotate-2 hover:scale-105 " />
                 </div>
             </section>
-            {children}
+            <section className="flex flex-1 flex-col items-center bg-white p-4 py-10 lg:justify-center lg:p-10 lg:py-0">
+                <div className="mb-16 lg:hidden">
+                    <Image
+                        src="/assets/icons/logo-full-brand.svg"
+                        alt="logo"
+                        width={224}
+                        height={82}
+                        className="h-auto w-[200px] lg:w-[250px]"
+                    />
+                </div>
+
+                {children}
+            </section>
         </div>
     )
 }
