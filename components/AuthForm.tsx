035fbac2bd2359ddfab905b2972711a38e54cae9
DPRAHUL <dpranavr2021@gmail.com>
2025-01-07 23:49:47 +0530

    Shadcn installed

---
+++ components/AuthForm.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Formtype = "sign-in" | "sign-up";
+
+const AuthForm = ({type} : {type:Formtype}) => {
+  return <div>AuthForm</div>;
+};
+
+export default AuthForm;
